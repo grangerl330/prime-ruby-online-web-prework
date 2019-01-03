6fc0c38aa6b71be449692af6352231fbe71ed5ea
Luke Granger <grangerl330@gmail.com>
2019-01-03 00:22:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(integer)
   if integer >= 2
   test_array = [2..integer - 1]
-  (2..integer - 1).each do |integer|
+  test_array.each do |integer|
     |x| integer % x != 0
   end
   else
